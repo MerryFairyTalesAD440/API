921ec96c9fa117ddfd0dfe08652b3e03d3f67963
Brad Nissen <bradanissen@gmail.com>
2019-02-24 10:37:43 -0800

    fixed bug, added logging

--- CreateContainer.cs
+++ CreateContainer.cs
@@ -1,21 +1,16 @@
-
-using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
-using System.Collections.Generic;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.KeyVault.Models;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
 
 
 namespace CreateContainer
@@ -26,28 +21,28 @@ namespace CreateContainer
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, ILogger log)
         {
-            // "name" is the query string -> ?name=
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            ProcessAsync(name).GetAwaiter().GetResult();
+            log.LogInformation("starting container build function.");
+            ProcessAsync(name, log).GetAwaiter().GetResult();
 
 
             return name != null
                 ? (ActionResult)new OkObjectResult("Container created with the name \"" + name + "\"")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-
-
         }
 
-        private static async Task ProcessAsync(string containerName)
+
+        private static async Task ProcessAsync(string containerName, ILogger log)
         {
             CloudStorageAccount storageAccount = null;
             CloudBlobContainer cloudBlobContainer = null;
 
+            log.LogInformation("In ProcessAsync function, attemping to access Key Vault.");
             // Code to grab the key from the Key Vault.
             var azureServiceTokenProvider = new AzureServiceTokenProvider();
             var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
@@ -60,6 +55,7 @@ namespace CreateContainer
             {
                 try
                 {
+                    log.LogInformation("Key Vault accessed.");
                     // Create the CloudBlobClient that represents the Blob storage endpoint for the storage account.
                     CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
 
@@ -80,18 +76,14 @@ namespace CreateContainer
                 }
                 catch (StorageException ex)
                 {
-                    // Fix - log this instead 
-                    Console.WriteLine("Error returned from the service: {0}", ex.Message);
+                    log.LogInformation("Error returned from the service: {0}", ex.Message);
                 }
 
             }
             else
             {
-                    // fix - log this instead
-                Console.WriteLine(
-                    "A connection string has not been defined in the system environment variables. " +
-                    "Add a environment variable named 'storageconnectionstring' with your storage " +
-                    "connection string as a value.");
+                log.LogInformation("Key Vault access failed. A connection string has not been " +
+                                    "defined in the system environment variables.");
             }
         }
 
