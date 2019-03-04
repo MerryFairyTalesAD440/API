d2c7253ba379ed306a229f7aad7067e1cab57351
mviencek <32376631+mviencek@users.noreply.github.com>
2019-03-04 13:08:39 -0800

    Fixing bug mikal (#68)
    * Create StartMeAPI
    
    * added .travis.yml file for deployment to azure functions
    
    * Revert "added .travis.yml file for deployment to azure functions"
    
    This reverts commit 1f5447962279bb98ff890797085b9e8c7df307f2.
    
    * added .travis.yml file for deployment to azure functions
    
    * modified travis
    
    * Initial commit of Post Book Function files
    
    * Added id to book
    
    I generate a GUID for the book id in the function, and that is then used as the id for Cosmos DB. This id is what will be used to retrieve the book later. The call returns the JSON book object, including the id.
    
    * github issues
    
    * fixing bug
    
    * forgot the change namespace
    
    * updating prefix
    
    * fixing spelling error
    
    * adjusting key valut code to use json
    
    * Delete merryfairytales - Zip Deploy.pubxml
    
    * adding getBooks function
    
    * Updated function to remove connection to my temp cosmos db
    
    * sql api read attempt with classes
    
    * updated comments
    
    * updating comments
    
    * updating comments
    
    * updating comments
    
    * updating comments
    
    * updating comments
    
    * variable names
    
    * Update getSAS.cs
    
    * add create container function the the dev branch
    
    * Update getSAS.cs
    
    return 405 if any method other than a post is sent
    
    * only allow post methods.  return 405 for everything else
    
    * add writeDoc
    
    * Update .gitignore
    
    adding local.settings to gitignore
    
    * get the lasted version of dev
    
    * adding .pubxml to gitignore
    
    * adding post and put and missing title from book object
    
    * testing travis ci
    
    * adding post and put
    
    * adjusting flow of post put
    
    * Mikal pages languages (#49)
    
    * sql api read attempt with classes
    
    * updated comments
    
    * updating comments
    
    * updating comments
    
    * updating comments
    
    * updating comments
    
    * updating comments
    
    * variable names
    
    * only allow post methods.  return 405 for everything else
    
    * adding post and put and missing title from book object
    
    * testing travis ci
    
    * adding post and put
    
    * adjusting flow of post put
    
    * Adding in Key Vault and Application Configs
    
    * adding in key vault
    
    * move key vault uri to app settings
    
    * remove values in the logs
    
    * get rid of unused packages
    
    * add documentation for methods, and clean up code
    
    * pull data from cosmosDB with title as query string
    
    * adding in reading a post or put instead of reading my test file
    
    * pages showing for GET
    
    * displaying pages with an id -> books/{bookid}/pages
    
    * add language function
    
    * add get pages and languages function to the api
    
    * adding second db call to handle when a book only has one page and language.  sql for cosmos is weird
    
    * cleaning up messages
    
    * change query to only search for a bookid
    
    * updating searching for a book functionality.  not quite there but close
    
    * forgot try catches.  done for the night
    
    * init commit for post/put texturl
    
    * adding comments
    
    * moving things around
    
    * fixing page error
    
    * change error returns and file names to be more specific
    
    * add http error codes 404 and 400 for either a bad request or resource not found
    
    * updates
    
    * finishing touches on GETs, DELETEs still in prog
    
    * add some finishing touches on GETs, DELETEs still in prog
    
    * live branch
    
    * adding route/book verification
    
    * modified .travis.yml file, removed deployment to master
    
    * Creating settings.json
    
    Adding settings.json to test deployment through Travis CI
    
    * Delete settings.json
    
    Didn't work...
    
    * update travis.yml
    
    * fixing bug
    
    * added branch dependancy
    
    * travis stuff again
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * fixed travis to deploy to specific branches
    
    * GET page/languages are work. DELETE languages is working
    
    * DEL pages works
    
    * changing sas error message.  getsastoken is no longer a valid error message
    
    * fixing bug brad found
    
    * fixing bug brad found
    
    * fixing bug brad found
    
    * adjusting comments
    
    * comments

---
+++ Functions/Book.cs
@@ -0,0 +1,37 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functions
+{
+    [JsonObject]
+    class Book
+    {
+        [JsonProperty(PropertyName = "id")]
+        public String Id { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public String Description { get; set; }
+
+        [JsonProperty(PropertyName = "author")]
+        public String Author { get; set; }
+
+        [JsonProperty(PropertyName = "cover_image")]
+        public String Cover_Image { get; set; }
+
+        [JsonProperty(PropertyName = "title")]
+        public String Title { get; set; }
+
+        [JsonConverter(typeof(StringConverter<Page>))]
+        [JsonProperty(PropertyName = "pages")]
+        public List<Page> Pages { get; set; }
+
+        public override string ToString()
+        {
+           return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+    }
+}
